d325f565a2bef087c7c0909dadc155c61c5273f2
chudadhakal <chudadhakal1@gmail.com>
2026-04-05 16:10:18 -0400

    added endpoint to add new application

--- JobApplicationTracker.WebApi/Controllers/ApplicationsController.cs
+++ JobApplicationTracker.WebApi/Controllers/ApplicationsController.cs
@@ -1,6 +1,8 @@
-﻿using JobApplicationTracker.WebApi.Models;
+﻿using JobApplicationTracker.WebApi.Data;
+using JobApplicationTracker.WebApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace JobApplicationTracker.WebApi.Controllers
 {
@@ -8,13 +10,35 @@ namespace JobApplicationTracker.WebApi.Controllers
     [ApiController]
     public class ApplicationsController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+        // constructor 
+        public ApplicationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpPost]
         [Route("CreateApplication")]
-        public IActionResult CreateApplication([FromQuery] ApplicationRequestModel model)
+        public async Task<IActionResult> CreateApplication(ApplicationRequestModel model)
         {
             //validate parameter
             if (!ModelState.IsValid) { return BadRequest(ModelState); }
-            return Ok(model);
+            // convert model to entity
+            var application = new JobApplication
+            {
+                CompanyName = model.CompanyName,
+                Position = model.JobTitle,
+                ApplicationDate = model.ApplicationDate,
+                Status = model.Status,
+                Notes = model.Notes
+            };
+
+            // add to database
+            _context.Add(application);
+            // save changes
+            await _context.SaveChangesAsync();
+
+            return Ok(application);
         }
     }
 }
